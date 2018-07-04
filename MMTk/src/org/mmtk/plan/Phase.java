d2944c9c6e586c539d90353392f3af48f3a531d0
Wenyu Zhao <wenyuzhaox@gmail.com>
2018-07-04 10:06:16 +1000

    Remove timing code for phases

--- MMTk/src/org/mmtk/plan/Phase.java
+++ MMTk/src/org/mmtk/plan/Phase.java
@@ -484,13 +484,7 @@ public abstract class Phase {
           if (logDetails) Log.writeln(" as Global...");
           if (primary) {
             if (VM.DEBUG) VM.debugging.globalPhase(phaseId,true);
-            long time = VM.statistics.nanoTime();
             plan.collectionPhase(phaseId);
-            Log.write("Global ");
-            Log.write(getName(phaseId));
-            Log.write(" ");
-            Log.write(VM.statistics.nanosToMillis(VM.statistics.nanoTime() - time));
-            Log.writeln(" ms");
             if (VM.DEBUG) VM.debugging.globalPhase(phaseId,false);
           }
           break;
@@ -500,13 +494,7 @@ public abstract class Phase {
         case SCHEDULE_COLLECTOR: {
           if (logDetails) Log.writeln(" as Collector...");
           if (VM.DEBUG) VM.debugging.collectorPhase(phaseId,order,true);
-          long time = VM.statistics.nanoTime();
           collector.collectionPhase(phaseId, primary);
-          Log.write("Collector ");
-          Log.write(getName(phaseId));
-          Log.write(" ");
-          Log.write(VM.statistics.nanosToMillis(VM.statistics.nanoTime() - time));
-          Log.writeln(" ms");
           if (VM.DEBUG) VM.debugging.collectorPhase(phaseId,order,false);
           break;
         }
@@ -515,18 +503,12 @@ public abstract class Phase {
         case SCHEDULE_MUTATOR: {
           if (logDetails) Log.writeln(" as Mutator...");
           /* Iterate through all mutator contexts */
-          long time = VM.statistics.nanoTime();
           MutatorContext mutator;
           while ((mutator = VM.activePlan.getNextMutator()) != null) {
             if (VM.DEBUG) VM.debugging.mutatorPhase(phaseId,mutator.getId(),true);
             mutator.collectionPhase(phaseId, primary);
             if (VM.DEBUG) VM.debugging.mutatorPhase(phaseId,mutator.getId(),false);
           }
-          Log.write("Mutator ");
-          Log.write(getName(phaseId));
-          Log.write(" ");
-          Log.write(VM.statistics.nanosToMillis(VM.statistics.nanoTime() - time));
-          Log.writeln(" ms");
           break;
         }
 
